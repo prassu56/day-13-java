8b04365f293b789cf2c906fd4ea76eb62b749ad3
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-14 15:15:24 +0530

    Add files via upload

---
+++ Creatingnewexceptionusingmethod.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Creatingnewexceptionusingmethod{
+   public static void a(int a,int b){
+        if(b==0)
+        {
+            throw new ArithmeticException("cannot divide");
+        }
+        System.out.println(a/b);
+    }
+    public static void main(String[]args){
+        try{
+            a(10,0);
+        }catch(ArithmeticException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
