7ed3469334d6eccc204db32eded054b95e791343
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-14 15:22:39 +0530

    Add files via upload

---
+++ Throws.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Throws{
+    public static void main(String[]args)throws ArithmeticException{
+        int a=10;
+        int b=0;
+        System.out.println(a/b);
+        //we will get error as out put
+    }
+}
